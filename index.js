11b2779f895124104e4ca9af033b58f04171174e
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-08-16 22:36:14 -0400

    Add js to render page when button is clicked

---
+++ index.js
@@ -0,0 +1,52 @@
+const form = document.getElementById("translation-form")
+const inputTextField = document.getElementById('input-txt')
+const submitBtn = document.getElementById("submit")
+const innerFormSection = document.getElementById('inner-form')
+const selectLangTitle = document.getElementById('select-lang-title')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const inputValue = inputTextField.value
+    if (submitBtn.value == "Translate"){
+        if (inputValue) {
+            // API functionality goes here
+            console.log(inputValue)
+            renderNewPage()
+        }
+    } 
+    else if (submitBtn.value == "Start Over"){
+        renderPreviousPage()
+    }
+
+})
+
+function renderNewPage(){
+    document.getElementById('translation-title').textContent = "Original text 👇" 
+    selectLangTitle.textContent = "Your translation 👇" 
+    selectLangTitle.style.paddingBottom = 0
+    innerFormSection.innerHTML = `<p id="output-txt" class="output-txt">Comment allez-vous?</p>`
+    submitBtn.value = "Start Over" 
+}
+
+function renderPreviousPage(){
+    document.getElementById('translation-title').textContent = "Text to translate 👇" 
+    selectLangTitle.textContent = "Select language 👇" 
+    inputTextField.value = ""
+    innerFormSection.innerHTML = `
+    <div>
+        <input type="radio" id="french" name="language" value="french" checked />
+        <label for="french">French </label>
+        <img src="assets/fr-flag.png" alt="Flag of France">
+    </div>
+    <div>
+        <input type="radio" id="spanish" name="language" value="spanish"/>
+        <label for="spanish">Spanish</label>
+        <img src="assets/sp-flag.png" alt="Flag of spain">
+    </div>
+    <div>
+        <input type="radio" id="japanese" name="language" value="japanese"/>
+        <label for="japanese">Japanese</label>
+        <img src="assets/jpn-flag.png" alt="Flag of Japan">
+    </div>
+    `
+    submitBtn.value = "Translate" 
+}
