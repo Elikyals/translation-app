80c0a345ae7ba9e92f8b2f7d89e6cd83f206a7b2
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-09-04 12:56:35 -0400

    Refactor API chat endpoint to include language and sentence parameters, update system prompt, and improve error handling

--- server.js
+++ server.js
@@ -1,36 +1,58 @@
 import express from 'express'
 import OpenAI from 'openai'
 import cors from 'cors'
+import 'dotenv/config'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const PORT = 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(__dirname))
+
 const openai = new OpenAI({
-    baseURL: 'https://api.deepseek.com',
+    baseURL: 'https://api.deepseek.com/v1',
     apiKey: process.env.DEEP_SEEK_API_KEY
 })
 
 app.post('/api/chat', async (req, res) => {
     try {
-        const { message } = req.body
+        const { language, sentence } = req.body
         const response = await openai.chat.completions.create({
             model: "deepseek-chat",
             messages: [
                 {
                     role: "system",
-                    content: ""//TODO: System prompt goes here
+                    content: `You are a pollyGlot, expert in french, spanish and japanese. 
+                            Translate the given statement to the languages you specialise in.
+                            Try to be concise as possible.`
                 },
                 {
                     role: "user",
-                    content: message
+                    content: `Translate the following from English to ${language}: 
+                            ${sentence}`
                 }
-            ]
+            ],
         })
         res.json({ reply: response.choices[0].message.content})
     } catch (error) {
-        res.status(500).json({error: 'Something went wrong'})
+        res.status(500).json({
+            error: 'Something went wrong',
+            message: error
+        })
     }
 })
 
-app.listen(3000)
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
